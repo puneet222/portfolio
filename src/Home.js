b8e19f90ca675a692b48a80e75c9354c3171018a
puneet222 <puneetaggarwal@pnplabs.in>
2017-05-07 14:45:30 +0530

    background video in computer and tablets

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactDriveIn from 'react-drive-in' ;
+import backgroundVideo from '../video/out-5.ogv' ;
+import backgroundImage from '../images/hny.png' ;
 
 class HomePage extends React.Component {
 
@@ -7,11 +10,14 @@ class HomePage extends React.Component {
   }
 
   render() {
+    var $mountNode = document.getElementById('drive-in');
     return (
-      <div id='homPage'>
-        <h1>
-          This is home page.
-        </h1>
+      <div id='homePage'>
+        <ReactDriveIn
+        show={backgroundVideo}
+        poster={backgroundImage}
+        />,
+        $mountNode
       </div>
     );
   }
