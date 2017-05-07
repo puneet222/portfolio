1338c4b35d2d4d3d23017668a89bf4b58fb06b54
puneet222 <puneetaggarwal@pnplabs.in>
2017-05-07 14:45:07 +0530

    bottom menu state changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 // import SelectedFoods from './SelectedFoods';
-// import FoodSearch from './FoodSearch';
+import FoodSearch from './FoodSearch';
 import Home from './Home' ;
+import classNames from 'classnames';
 
 class App extends Component {
-  oldState = {
-    selectedFoods: [],
-  }
-
-  state = {
-    current : "home",
+  constructor(props) {
+    super(props);
+    this.state = {
+      iconsLeftPadding : 0,
+      homeState : false,
+      aboutState : false,
+      skillsState : false,
+      projectState : false,
+      internshipState : false,
+      contactState : false,
+      videoURL : "https://www.dropshots.com/video.php?u=https%3A%2F%2Fstorage04.dropshots.com%2Fphotos6000%2Fphotos%2F1388321%2F20170506%2F070642.mp4"
+    };
   }
 
   // removeFoodItem = (itemIndex) => {
@@ -24,8 +31,122 @@ class App extends Component {
   //   this.setState({ selectedFoods: newFoods });
   // }
 
+  onHover = () => {
+    console.log("on hover") ;
+  }
+
+  onOut = () => {
+    console.log("out") ;
+  }
+
+  handleHomeClick = () => {
+    this.setState({
+      homeState : true,
+      aboutState : false,
+      skillsState : false,
+      projectState : false,
+      internshipState : false,
+      contactState : false
+    })
+  }
+
+  handleAboutClick = () => {
+    this.setState({
+      homeState : false,
+      aboutState : true,
+      skillsState : false,
+      projectState : false,
+      internshipState : false,
+      contactState : false
+    })
+  }
+
+  handleSkillsClick = () => {
+    this.setState({
+      homeState : false,
+      aboutState : false,
+      skillsState : true,
+      projectState : false,
+      internshipState : false,
+      contactState : false
+    })
+  }
+
+  handleProjectClick = () => {
+    this.setState({
+      homeState : false,
+      aboutState : false,
+      skillsState : false,
+      projectState : true,
+      internshipState : false,
+      contactState : false
+    })
+  }
+
+  handleInternshipClick = () => {
+    this.setState({
+      homeState : false,
+      aboutState : false,
+      skillsState : false,
+      projectState : false,
+      internshipState : true,
+      contactState : false
+    })
+  }
+
+  handleContactClick = () => {
+    this.setState({
+      homeState : false,
+      aboutState : false,
+      skillsState : false,
+      projectState : false,
+      internshipState : false,
+      contactState : true
+    })
+  }
+
   render() {
     // const { selectedFoods } = this.oldState;
+
+    var style = {
+      "marginLeft" : 'auto',
+      'marginRight' : "auto",
+      'paddingLeft' : this.state.iconsLeftPadding + "px"
+    };
+
+    var iconPadding = {
+      'padding' : '10px'
+    };
+
+    var homeClass = classNames({
+      "item" : true,
+      "active" : this.state.homeState
+    });
+
+    var aboutClass = classNames({
+      "item" : true ,
+      "active" : this.state.aboutState
+    });
+
+    var skillsClass = classNames({
+      "item" : true ,
+      "active" : this.state.skillsState
+    });
+
+    var projectClass = classNames({
+      "item" : true ,
+      "active" : this.state.projectState
+    });
+
+    var internshipClass = classNames({
+      "item" : true ,
+      "active" : this.state.internshipState
+    });
+
+    var contactClass = classNames({
+      "item" : true ,
+      "active" : this.state.contactState
+    });
 
     return (
       <div className='App'>
@@ -35,21 +156,66 @@ class App extends Component {
           </video>
         </div>
         <div className='ui text container'>
-          <Home />
+        {
+          this.state.home
+            ? <FoodSearch />
+            : null
+        }
+          <Home videoURL={this.state.videoURL}/>
         </div>
-        <div className="ui menu" id="footer">
-          <div className="fitted item">
-            No padding whatsoever
+        <div className="ui inverted segment" id="footer">
+        {/* ------------------------------  this is division for computer and tablets ------------------------------- */}
+          <div className="ui compact inverted secondary pointing menu computer only grid tablet only grid">
+            <a className={homeClass} onClick={this.handleHomeClick}>
+              <span>Home</span>
+            </a>
+            <a className={aboutClass} onClick={this.handleAboutClick}>
+              About
+            </a>
+            <a className={skillsClass} onClick={this.handleSkillsClick}>
+              Skills
+            </a>
+            <a className={projectClass} onClick={this.handleProjectClick}>
+              Projects
+            </a>
+            <a className={internshipClass} onClick={this.handleInternshipClick}>
+              Internships
+            </a>
+            <a className={contactClass} onClick={this.handleContactClick}>
+              Contact
+            </a>
           </div>
-          <div className="horizontally fitted item">
-            No horizontal padding
-          </div>
-          <div className="vertically fitted item">
-            No vertical padding
-          </div>
+          {/* ------------------------------  this is division for mobiles ------------------------------- */}
+            <div className="ui compact inverted secondary pointing menu mobile only grid" style={style} id="icons">
+              <a className={homeClass} style={iconPadding} onClick={this.handleHomeClick} >
+                <h2><i className="home icon"></i></h2>
+              </a>
+              <a className={aboutClass} style={iconPadding} onClick={this.handleAboutClick}>
+                <h2><i className="info circle icon"></i></h2>
+              </a>
+              <a className={skillsClass} style={iconPadding} onClick={this.handleSkillsClick}>
+                <h2><i className="terminal icon"></i></h2>
+              </a>
+              <a className={projectClass} style={iconPadding} onClick={this.handleProjectClick}>
+                <h2><i className="code icon"></i></h2>
+              </a>
+              <a className={internshipClass} style={iconPadding} onClick={this.handleInternshipClick}>
+                <h2><i className="cubes icon"></i></h2>
+              </a>
+              <a className={contactClass} style={iconPadding} onClick={this.handleContactClick}>
+                <h2><i className="mail icon"></i></h2>
+              </a>
+            </div>
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    var divWidth = document.getElementById("icons").offsetWidth ;
+    var windowWidth = window.innerWidth ;
+    var leftPadding = (windowWidth-divWidth)/2 ;
+    this.setState({iconsLeftPadding : leftPadding});
   }
 }
 
