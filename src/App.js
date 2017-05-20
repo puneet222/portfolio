9c3f2fd5e8cea474f1dae30405d561bad2113aa5
puneet222 <aggarwal.puneet222@gmail.com>
2017-05-20 12:13:56 +0530

    typo

--- src/App.js
+++ src/App.js
@@ -199,7 +199,7 @@ class App extends Component {
 
         <div className="temporary" id="temp">
           <div id="temporaryContent" style={{"textAlign" : "center" , "marginTop" : "15vh"}}>
-            <h3 className="center homeContent" style={{"color" : "#00E5FF" , "fontSize" : "4vh" , "fontWeight" : "100"}}>Resonse Time : {this.state.responseTime}ms</h3>
+            <h3 className="center homeContent" style={{"color" : "#00E5FF" , "fontSize" : "4vh" , "fontWeight" : "100"}}>Response Time : {this.state.responseTime}ms</h3>
             <h3 className="center homeContent" style={{"color" : "#00E5FF" , "fontSize" : "10vh" , "fontWeight" : "100"}}>&beta; version</h3>
             <h3 className="center" style={{"color" : "white" , "fontSize" : "3.5vh" , "fontWeight" : "100"}}>You need an access to view the amazing content</h3>
             <br />
